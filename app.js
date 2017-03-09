608716819bfd02032d76ee3ed886aa9f51a119ca
Zach Marcolesco <zach.marcolesco@gmail.com>
2017-03-09 13:25:20 -0600

    updated app

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ var sqlite3 = require('sqlite3');
 
 var db = new sqlite3.Database('scrumtastic.sqlite3');
 
-var migrate = require('./src/migrations');
-migrate(db);
-
-db.run("INSERT INTO projects (name) values ('title')");
-console.log(db.run("SELECT * FROM projects"));
+var migrate = require('./lib/migrate');
+migrate(db, 'migrations', function(err) {
+  db.serialize(function() {
+    db.run("INSERT INTO projects (name) values (?)", ['title']);
+    db.all("SELECT * FROM projects", [], function(err, rows) {
+      if(err) return console.error(err);
+      console.log("ROWS:", rows);
+    });
+  });
+});
